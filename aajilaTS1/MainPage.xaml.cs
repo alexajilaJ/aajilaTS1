efd5c1ae35c247771a33b27692263f957054bb8b
Alex <Alex@DESKTOP-DNAHFBI>
2024-04-08 00:12:56 -0500

    Actualización

--- aajilaTS1/MainPage.xaml.cs
+++ aajilaTS1/MainPage.xaml.cs
@@ -20,15 +20,6 @@
             DisplayAlert(" ", "Bienvenido " + nombre + " " + apellido + " \ntienes " + edad + " años" + " \ntu aporte mensual es " + aporte, "Cerrar");
         }
 
-        /*private void ObtnCalcular_Clicked(object sender, EventArgs e)
-        {
-            string nombre = txtNombre.Text;
-            string apellido = txtApellido.Text;
-            int edad = Convert.ToInt32(txtEdad.Text);
-            int salario = Convert.ToInt32(txtSalario);
-            double aporte = salario * (9.45 / 100);
-            DisplayAlert(" ","Bienvenido " + nombre + " "+ apellido +" \ntienes "+ edad + " años"+" \ntu aporte mensual es "+aporte, "Cerrar");
-        }*/
     }
 
 }
