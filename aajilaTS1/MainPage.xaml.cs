6cf8a03ee40278a1eaa3cd45fd7bdd6323a0e136
Alex <Alex@DESKTOP-DNAHFBI>
2024-04-08 00:06:17 -0500

    Agregar archivos de proyecto.

---
+++ aajilaTS1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace aajilaTS1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int edad = Convert.ToInt32(txtEdad.Text);
+            int salario = Convert.ToInt32(txtSalario.Text);
+            double aporte = salario * 0.0945;
+
+            DisplayAlert(" ", "Bienvenido " + nombre + " " + apellido + " \ntienes " + edad + " años" + " \ntu aporte mensual es " + aporte, "Cerrar");
+        }
+
+        /*private void ObtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int edad = Convert.ToInt32(txtEdad.Text);
+            int salario = Convert.ToInt32(txtSalario);
+            double aporte = salario * (9.45 / 100);
+            DisplayAlert(" ","Bienvenido " + nombre + " "+ apellido +" \ntienes "+ edad + " años"+" \ntu aporte mensual es "+aporte, "Cerrar");
+        }*/
+    }
+
+}
---
+++ aajilaTS1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace aajilaTS1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
